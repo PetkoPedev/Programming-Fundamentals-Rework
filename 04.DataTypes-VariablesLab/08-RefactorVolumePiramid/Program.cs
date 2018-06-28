9580dc2ed0189126ca4e1b09a9c89e4986e05840
Petko Pedev <petko.pedev@gmail.com>
2018-06-28 22:42:28 +0300

    PF Data variables rework.

---
+++ 04.DataTypes-VariablesLab/08-RefactorVolumePiramid/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08_RefactorVolumePiramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double length, width, volume = 0;
+            Console.Write("Length: ");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            double volume = double.Parse(Console.ReadLine());
+            volume = (length * width * volume) / 3;
+            Console.WriteLine("Pyramid Volume: {0:F2}", volume);
+        }
+    }
+}
