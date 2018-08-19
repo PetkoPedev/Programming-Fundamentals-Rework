a6e8e34519d1d832f8946cf713fab84389017fa9
Petko Pedev <petko.pedev@gmail.com>
2018-08-19 08:52:35 +0300

    Dictionaries lab added.

---
+++ 08.Dictionaries-Lab/P01-CountRealNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            var counts = new SortedDictionary<double, int>();
+            foreach (var num in nums)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+            foreach (var num in counts.Keys)
+            {
+                Console.WriteLine($"{num} -> {counts[num]}");
+            }
+        }
+    }
+}
